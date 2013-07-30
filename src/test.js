656ce491c69e22425e5aac46b74baa56f89fdf83
ip2location-nodejs <ip2location.nodejs@gmail.com>
2013-07-30 14:43:20 +0800

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,4 +1,4 @@
-var ip2loc = require("ip2location");
+var ip2loc = require("ip2location-nodejs");
 
 // for both IPv4 and IPv6
 ip2loc.IP2Location_init("/root/testnodejs/db24.BIN", "/root/testnodejs/IPV6-COUNTRY.BIN");
@@ -11,7 +11,7 @@ ip2loc.IP2Location_init("/root/testnodejs/db24.BIN", "/root/testnodejs/IPV6-COUN
 
 testip = ['8.8.8.8', '2404:6800:4001:c01::67', '2001:0200:0102:0000:0000:0000:0000:0000', '2001:0200:0135:0000:0000:0000:0000:0000', '2001:0200:017A:0000:0000:0000:0000:0000'];
 for (var x = 0; x < testip.length; x++) {
-  result = ip2loc.IP2Location_get_all(testip[x]);
+	result = ip2loc.IP2Location_get_all(testip[x]);
 	for (var key in result) {
 		console.log(key + ": " + result[key]);
 	}
