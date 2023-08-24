e44e66c443dd25fa85e7e52f857bfe8455772ba7
ip2location <support@ip2location.com>
2023-08-24 14:42:57 +0800

    Update the theme used.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 # Configuration file for the Sphinx documentation builder.
 # Read https://www.sphinx-doc.org/en/master/usage/configuration.html for more options available
 
-import sphinx_pdj_theme
+# import sphinx_pdj_theme
 
 # -- Project information
 
@@ -21,6 +21,13 @@ extensions = [
     'sphinx_copybutton',
 ]
 
+# https://myst-parser.readthedocs.io/en/latest/configuration.html#setting-html-metadata
+myst_html_meta = {
+    "description": "IP2Proxy PHP library enables user to query an IP address if it was being used as open proxy, web proxy, VPN anonymizer and TOR exits.",
+    "keywords": "IP2Location, Geolocation, IP location",
+    "google-site-verification": "DeW6mXDyMnMt4i61ZJBNuoADPimo5266DKob7Z7d6i4",
+}
+
 # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html
 
 myst_enable_extensions = [
@@ -33,13 +40,16 @@ myst_enable_extensions = [
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_pdj_theme'
-html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
+html_theme = 'sphinx_book_theme'
+# html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 
 # PDJ theme options, see the list of available options here: https://github.com/jucacrispim/sphinx_pdj_theme/blob/master/sphinx_pdj_theme/theme.conf
 html_theme_options = {
-    # Hide the name of the project above the search bar
-    'home_link': 'hide'
+    "use_edit_page_button": False,
+    "use_source_button": False,
+    "use_issues_button": False,
+    "use_download_button": False,
+    "use_sidenotes": False,
 }
 
 # The name of an image file (relative to this directory) to place at the top
@@ -49,4 +59,6 @@ html_logo = 'images/ipl-logo-square-1200.png'
 # Favicon
 html_favicon = 'images/favicon.ico'
 
-html_title = "IP2Location Python"
+html_title = "IP2Location NodeJS"
+
+html_baseurl = "https://ip2location-nodejs.readthedocs.io/en/latest/"
